fa95514b8a4e292e6ca03c50e57af7d5ee12b387
bChakrabarty <biswapriyo.chakrabarty@gmail.com>
2022-05-25 17:11:46 -0700

    SQUASHED v3

---
+++ Assets/Recorder.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Recorder : MonoBehaviour
+{
+	public SessionRecorderBehaviour script;
+
+	public void StartRecording()
+	{
+
+	}
+}
